00bfa70f513bdd13f0c0c34ed7ea98aa92f9664a
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-09-05 08:21:40 +0800

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+num1 = 2;
+num2 = 5;
+sum = num1 + num2;
+
+console.log("The sum of: " + num1 + "and" + num2 + "is" + sum);
+
+diff =num1 + num2;
+prod = num1 * num2;
+quot = num1 / num2;
+
+console.log("The difference of " + num1 + "&" + num2 + "is" + diff);
+console.log("The product of " + num1 + "&" + num2 + "is" + prod);
+console.log("The quotient of " + num1 + "&" + num2 + "is" + quot);
