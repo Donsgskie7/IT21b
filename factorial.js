c71f894bb5413e93a79cb53214c2d4be097a8d3d
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-09-09 22:18:16 -0800

    added mainMenu Function & initial instance

--- factorial.js
+++ factorial.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 
     function mainMenu(){
     console.log("------------------------------");
-    console.log("Factorial Application");
+    console.log(" Factorial Application ");
     console.log("1. Say Hello");
     console.log("2. Factorial");
     console.log("3. Exit Application");
