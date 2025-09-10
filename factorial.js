cf0ba57defa248d8148a4ecab264f33b20574920
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-09-09 22:07:45 -0800

    const readline=require(readline);

--- factorial.js
+++ factorial.js
@@ -1,28 +1,29 @@
 const readline=require("readline");
 
-const r1 = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-console.log("Factorial Application");
-console.log("------------------------------");
-console.log("1. Say Hello");
-console.log("2. Factorial");
-console.log("3. Exit Application");
-console.log("------------------------------");
+    function mainMenu(){
+    console.log("------------------------------");
+    console.log("Factorial Application");
+    console.log("1. Say Hello");
+    console.log("2. Factorial");
+    console.log("3. Exit Application");
+    console.log("------------------------------");
 
-r1.question("enter your choice (1-3): ", choice =>{
-    console.log(choice);
+    rl.question("enter your choice (1-3): ", choice =>{
+    
+    if (choice === "1"){
+       sayHello();
+    } else if (choice === "2"){
+        computeFactorial();
+    } else if (choice === "3"){
+           exitProgram();
+    } else {
+        console.log("Invalid Choice. Please Try Again");
+    }
 
-if (choice === "1"){
-    console.log("Hello");
-} else if (choice === "2"){
-    console.log("Factorial");
-} else if (choice === "3"){
-    console.log("Exiting Application");
-} else {
-    console.log("Invalid Choice. Please Try Again");
+    })
 }
-
-})
