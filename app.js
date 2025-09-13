d5edbd78f7e9680887f8ee66f3d02b12bc1c4107
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-09-12 20:24:05 -0800

    Added Class Person and DisplayInfo

---
+++ app.js
@@ -0,0 +1,27 @@
+class Person{
+    name;
+    age;
+    occupation;
+    message;
+
+    constructor(name, age, occupation, message){
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.message = message;
+
+    }
+
+    displayInfo(){
+        console.log("Name: "+ this.name);
+        console.log("Age: "+ this.age);
+        console.log("Occupation: "+ this.occupation);
+        console.log("Message: "+ this.message);
+    }
+}
+
+    const person1 = new Person("Coco Martin", 24, "Masahista", "Ser, tapos na po");
+    const person2 = new Person("Victor Magtanggol", 30, "Superhero", "arjooooooooo");
+
+    person1.displayInfo();
+    person2.displayInfo();
